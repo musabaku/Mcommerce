0fe1ec386ac14667c256551758845e6c0cb8fe11
Musab <musabkhan2003@gmail.com>
2023-07-23 17:54:03 +0300

    gg

--- MERN - E-Commerce/Backend/server.js
+++ MERN - E-Commerce/Backend/server.js
@@ -1,18 +1,30 @@
 const app = require("./app");
 // const dotenv = require("dotenv");
 const connectDatabase = require("./database");
-const cors = require("cors"); 
+// const cors = require("cors"); 
 // Config
 if (process.env.NODE_ENV !== "PRODUCTION") {
   require("dotenv").config({ path: "Backend/config/config.env" });
 }
 
-const corsOptions ={
-  origin:'*',
-    credentials:true,   
-    optionSuccessStatus:200,        
-}
-app.use(cors(corsOptions));
+// const corsOptions ={
+//   origin:'*',
+//     credentials:true,   
+//     optionSuccessStatus:200,        
+// }
+// app.use(cors(corsOptions));
+app.use(function(req, res, next) {
+  // res.header("Access-Control-Allow-Origin", "*");
+  const allowedOrigins = ['http://localhost:3000', 'https://macommerce-ndrj.onrender.com', 'https://illustrious-crepe-c524d2.netlify.app','https://64bd3a3ba116002a6b68077b--friendly-malasada-66c970.netlify.app'];
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+       res.setHeader('Access-Control-Allow-Origin', origin);
+  }
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  res.header("Access-Control-Allow-credentials", true);
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, UPDATE");
+  next();
+});
 connectDatabase();
 
 
