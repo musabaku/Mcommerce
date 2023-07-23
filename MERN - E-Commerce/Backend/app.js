1244f3c197648c9993d37d7a47d15c9e10aea7cf
Musab <musabkhan2003@gmail.com>
2023-07-23 10:14:00 +0300

    h

--- MERN - E-Commerce/Backend/app.js
+++ MERN - E-Commerce/Backend/app.js
@@ -14,18 +14,12 @@ const user = require("./routes/userRoute")
 app.use("/api/v1",product)
 app.use("/api/v1",user)
 
-// app.use(express.static(path.join(__dirname, "../frontend/build")));
-
-// app.get("*", (req, res) => {
-//   res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
-// });
-const buildPath = path.join(__dirname, "frontend", "build");
-
-app.use(express.static(buildPath));
+app.use(express.static(path.join(__dirname, "../frontend/build")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(buildPath, "index.html"));
+  res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
 });
+
 app.use(errorMiddleware)
 
 
